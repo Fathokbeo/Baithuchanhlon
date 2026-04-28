f7a3db883e96d94c33fb67b0716184c1d292748d
Tien <tien20052007@gmail.com>
2026-04-29 01:30:13 +0700

    cap nhat databaseManager and UserDao

--- src/main/server/dao/AuctionDao.java
+++ src/main/server/dao/AuctionDao.java
@@ -1,21 +1,3 @@
 package main.server.dao;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-public final class AuctionDao {
-    private final DatabaseManager databaseManager;
-
-    public AuctionDao(DatabaseManager databaseManager) {
-        this.databaseManager = databaseManager;
-    }
-
-    public List<Auction> findAll() {
+ public class AuctionDao{
+ }
