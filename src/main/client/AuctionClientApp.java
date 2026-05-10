a208ff23199a338b867a33a5db8063263c9c797c
Em Hào <haol7a@gmail.com>
2026-05-10 21:07:42 +0700

    Sửa AuctionClientApp để thêm logo cho app

--- src/main/client/AuctionClientApp.java
+++ src/main/client/AuctionClientApp.java
@@ -1,11 +1,16 @@
 package main.client;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public final class AuctionClientApp extends Application {
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.getIcons().add(
+                new Image(AuctionClientApp.class.getResourceAsStream("/images/app-icon.png"))
+        );
+
         AppContext.initialize(primaryStage);
         AppContext.showLoginView();
     }
